cc416370a42d816700605d7b54f2f63e016604fb
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2022-07-07 10:50:43 -0300

    feat: change converter cli constant

--- lib/epub2mobi.rb
+++ lib/epub2mobi.rb
@@ -5,7 +5,7 @@ require_relative 'epub2mobi/version'
 # Epub to Mobi file type converter
 # It's based on calibre-ebook converter, so you need to install it
 module Epub2mobi
-  CONVERT_APP = ENV.fetch("CONVERT_APP", "/usr/local/bin/ebook-convert")
+  CONVERT_APP = ENV.fetch('CONVERT_APP', 'ebook-convert')
   private_constant :CONVERT_APP
 
   def self.convert(from:, to:)
